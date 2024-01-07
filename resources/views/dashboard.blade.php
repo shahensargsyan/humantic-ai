cd31aaf9b2a9efeef8cc782221963bd01c0dd71b
collenteeru <155763556+collenteeru@users.noreply.github.com>
2024-01-07 01:14:10 +0100

    Update dashboard.blade.php
    export timer

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -55,7 +55,7 @@ use Illuminate\Support\Facades\Session;e html>
                     <p class="lead">
                         <input name="id" type="hidden" value="{{ $id }}">
                     </p>
-                    <button type="submit" class="btn btn-primary btn-lg">Export</button>
+                    <button type="submit" class="btn btn-primary btn-lg" id="exportButton">Export</button>
                 </form>
             </div>
         </div>
@@ -64,6 +64,17 @@ use Illuminate\Support\Facades\Session;e html>
 
 
 <!-- Optional JavaScript -->
+    <script>
+      window.onload = function() {
+        // Disable the button when the page loads
+        document.getElementById('exportButton').disabled = true;
+
+        // Enable the button after 10 seconds
+        setTimeout(function() {
+            document.getElementById('exportButton').disabled = false;
+        }, 15000);
+    };
+</script>
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
